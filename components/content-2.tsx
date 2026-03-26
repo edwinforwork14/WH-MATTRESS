a6b9af77fd0a4defa92e90922656c2372a4adc4f
edwinforwork14 <edwinforwork14@gmail.com>
2026-03-25 20:08:23 -0500

    ultimos detalles

--- components/content-2.tsx
+++ components/content-2.tsx
@@ -6,10 +6,10 @@ export default function ContentSection() {
         <div className='pb-35'>
         <section className="mx-2 sm:mx-4 lg:mx-10 py-12 md:py-16 font-sans glass rounded-[2.5rem] shadow-2xl">
             <div className="mx-auto max-w-5xl space-y-8 px-6 md:space-y-16 pb-12">
-                <h2 className="relative z-10 text-4xl font-semibold text-title lg:text-5xl">El ecosistema WH:<br />Ingeniería al servicio de tu sueño.</h2>
+                <h2 className="text-center z-10 text-4xl font-semibold text-title lg:text-5xl">El ecosistema WH:<br />Ingeniería al servicio de tu sueño.</h2>
                 <div className="relative">
                     <div className="relative z-10 space-y-4 md:w-1/2">
-                    <p> — desde el colchón hasta la almohada ergónomica, todo conectado para maximizar tu recuperación.</p>
+                    <p> — Desde el colchón hasta la almohada ergónomica, todo conectado para maximizar tu recuperación.</p>
                         <p>WH MATTRESS S.A. evoluciona más allá del colchón convencional. Creamos un ecosistema integral del sueño que fusiona tecnología de muelles ensacados y espuma viscoelástica con materiales de alta gama, para revitalizar tu cuerpo y mente noche tras noche.</p>
 
                         <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 pt-6 sm:gap-4">
